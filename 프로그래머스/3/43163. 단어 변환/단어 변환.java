7f110a58d7bcbf5266aa72da82ac4bdf7e286ed6
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-03-11 15:47:01 +0900

    [level 3] Title: 단어 변환, Time: 0.70 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,9 +1,8 @@
 import java.util.*;
 
 class Solution {
-
-    static class Word {
-
+    
+    public static class Word {
         String word;
         int change;
         
@@ -11,56 +10,46 @@ class Solution {
             this.word = word;
             this.change = change;
         }
-
+        
     }
-
+    
     public int solution(String begin, String target, String[] words) {
-
         int answer = 0;
         
-        int min = 0;
-
         Queue<Word> queue = new ArrayDeque<>();
         queue.add(new Word(begin, 0));
-
+        
         Set<String> selected = new HashSet<>();
         selected.add(begin);
-
+        
         while (!queue.isEmpty()) {
-
-            Word word = queue.poll();
-
             
-            if (word.word.equals(target)) {
-                min = word.change;
-            }
-
-            for (String s : words) {
+            Word current = queue.poll();
+            
+            String word = current.word;
+            int change = current.change;
+            
+            if (word.equals(target)) return change;
+            
+            for (String next : words) {
                 
-                if (selected.contains(s)) continue;
-
+                if (selected.contains(next)) continue;
+                
                 int count = 0;
-
-                char[] ch1 = word.word.toCharArray();
-                char[] ch2 = s.toCharArray();
-
-                for (int j = 0; j < ch1.length; j++) {
-                    if (ch1[j] != ch2[j]) count++;
+                
+                for (int i=0; i<next.length(); i++) {
+                    if (current.word.charAt(i)!=next.charAt(i)) count++;
                 }
-
-                if (count == 1) {
-                    queue.add(new Word(s, word.change+1));
-                    selected.add(s);
+                
+                if (count==1) {
+                    selected.add(next);
+                    queue.add(new Word(next, change+1));
                 }
-
+                
             }
-
+            
         }
-
-        answer = min;
-
+        
         return answer;
-
     }
-
 }
