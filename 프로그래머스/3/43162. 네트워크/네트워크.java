291b2bc95fe94aecf1d97474f232a043256afd23
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-03-11 12:15:53 +0900

    [level 3] Title: 네트워크, Time: 0.31 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,44 +1,32 @@
-import java.util.*;
-
 class Solution {
     
-    public void bfs(int node, int[][] computers, boolean[] visited) {
+    public void dfs(int node, int[][] computers, boolean[] visited) {
         
         if (visited[node]) return;
         
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.add(node);
         visited[node] = true;
         
-        while(!queue.isEmpty()) {
+        for (int i=0; i<computers[node].length; i++) {
             
-            int index = queue.poll();
-            
-            for (int i=0; i<computers[index].length; i++) {
-                    
-                if (index!=i && computers[index][i]==1 && !visited[i]) {
-                    visited[i] = true;
-                    queue.add(i);
-                }
-
+            if (node!=i && computers[node][i]==1 && !visited[i]) {
+                dfs(i, computers, visited);
             }
             
         }
         
     }
     
-    
     public int solution(int n, int[][] computers) {
         int answer = 0;
         
         int count = 0;
         boolean[] visited = new boolean[n];
-    
+        
         for (int i=0; i<n; i++) {
             
             if (!visited[i]) {
+                dfs(i, computers, visited);
                 count++;
-                bfs(i, computers, visited);
             }
             
         }
