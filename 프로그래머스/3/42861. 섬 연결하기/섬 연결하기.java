984d201498f4aded30684c7be3ab544cd146af6b
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-03-11 12:02:56 +0900

    [level 3] Title: 섬 연결하기, Time: 0.81 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,61 +1,59 @@
 import java.util.*;
 
 class Solution {
-
+    
     public int find(int[] arr, int i) {
-
+        
         if (arr[i]==i) return i;
-
+        
         return arr[i] = find(arr, arr[i]);
-
     }
-
+    
     public void union(int[] arr, int a, int b) {
-
+        
         int rootA = find(arr, a);
         int rootB = find(arr, b);
-
-        if (rootA != rootB) {
-
-            if (rootA < rootB) {
+        
+        if (rootA!=rootB) {
+            
+            if (rootA<rootB) {
                 arr[rootB] = rootA;
             }else {
                 arr[rootA] = rootB;
             }
-
+            
         }
-
-    }
-
-    public int solution(int n, int[][] costs) {
-            int answer = 0;
-
-            int min = 0;
-
-            Arrays.sort(costs, (n1, n2) -> n1[2]-n2[2]);
-
-            boolean[] visited = new boolean[n];
-            int[] networks = new int[n];
         
-            for (int i=0; i<n; i++) {
-                networks[i] = i;
-            }
-
-            for (int[] cost : costs) {
-                
-                int start = find(networks, cost[0]);
-                int end = find(networks, cost[1]);
-                
-                if (start!=end) {
-                    union(networks, start, end);
-                    min += cost[2];
-                }
-
-            }
-
-            answer = min;
-
-            return answer;
     }
-
+    
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        
+        int minExpense = 0;
+        
+        Arrays.sort(costs, (a, b)-> a[2]-b[2]);
+        
+        int[] networks = new int[n];
+        
+        for (int i=0; i<n; i++) {
+            networks[i] = i;
+        }
+        
+        for (int[] cost : costs) {
+            
+            int from = cost[0];
+            int to = cost[1];
+            int expense = cost[2];
+            
+            if (find(networks, from) != find(networks, to)) {
+                minExpense += expense;
+                union(networks, from ,to);
+            }
+            
+        }
+        
+        answer = minExpense;
+        
+        return answer;
+    }
 }
