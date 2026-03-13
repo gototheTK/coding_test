2c90620c89a0c428ae6caf4b5215a3df74327fb6
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-03-13 12:40:18 +0900

    [level 3] Title: 정수 삼각형, Time: 15.17 ms, Memory: 63.9 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -11,7 +11,7 @@ class Solution {
                 if (j==0) {
                     triangle[i][j] += triangle[i-1][j];
                 }else if (j==triangle[i].length-1) {
-                    triangle[i][j] += triangle[i-1][triangle[i-1].length-1];
+                    triangle[i][j] += triangle[i-1][j-1];
                 }else {
                     triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);
                 }
