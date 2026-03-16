d68261d13dbb1180932bb8f38523e9e6bb6a322f
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-03-16 11:07:58 +0900

    [level 3] Title: 등굣길, Time: 0.87 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int answer = 0;
@@ -8,31 +6,31 @@ class Solution {
         map[0][0] = 1;
         
         for (int[] puddle : puddles) {
-            int i=puddle[0]-1, j=puddle[1]-1;
-            map[i][j] = -1;
+            int x = puddle[0]-1, y = puddle[1]-1;
+            map[x][y] = -1;
         }
         
-        for (int i=0; i<map.length; i++) {
-            
-            for (int j=0; j<map[i].length; j++) {
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
                 
                 if (map[i][j]==-1) {
-                    map[i][j]=0;
+                    map[i][j] = 0;
                     continue;
-                } 
-                
-                if (j>0) {
-                    map[i][j] += map[i][j-1];
                 }
                 
                 if (i>0) {
                     map[i][j] += map[i-1][j];
                 }
                 
-                map[i][j]%=1_000_000_007;
+                map[i][j] %= 1000_000_007;
+                
+                if (j>0) {
+                    map[i][j] += map[i][j-1];
+                }
+                
+                map[i][j] %= 1000_000_007;
                 
             }
-            
         }
         
         answer = map[m-1][n-1];
