6828437c528d6a464d66382a5532bd5832e92b6a
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-03-16 13:14:30 +0900

    [level 3] Title: 입국심사, Time: 57.26 ms, Memory: 92.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        
+        long left = 1;
+        long right = (long) n*times[times.length-1];
+        
+        while (left<=right) {
+
+            long mid = (left+right)/2;
+            long count = 0;
+            
+            for (int i=0; i<times.length; i++) {
+                count += mid/times[i];
+            }
+            
+            if (count>=n) {
+                answer = mid;
+                right = mid-1;
+            }else {
+                left = mid+1;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
