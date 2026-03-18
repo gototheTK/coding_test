5342a2842ee93e2900409dcb363f7bc090018537
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-03-18 12:38:09 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 51.24 ms, Memory: 215 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -2,13 +2,16 @@ class Solution {
     public int solution(int[][] board, int[][] skill) {
         int answer = 0;
         
-        int n = board.length;
+        int count = 0;
+        
         int m = board[0].length;
+        int n = board.length;
+        
         int[][] dashboard = new int[n+1][m+1];
         
         for (int[] s : skill) {
             
-            int type = s[0]==2 ? 1 : -1;
+            int type = s[0] == 1 ? -1 : 1;
             int r1 = s[1], c1 = s[2];
             int r2 = s[3], c2 = s[4];
             int degree = type * s[5];
@@ -20,24 +23,22 @@ class Solution {
             
         }
         
-        for (int i=1; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                dashboard[i][j] += dashboard[i-1][j];
-            }
-        }
-        
         for (int i=0; i<n; i++) {
             for (int j=1; j<m; j++) {
                 dashboard[i][j] += dashboard[i][j-1];
             }
         }
         
-        int count = 0;
+        for (int j=0; j<m; j++) {
+            for (int i=1; i<n; i++) {
+                dashboard[i][j] += dashboard[i-1][j];
+            }
+        }
         
         for (int i=0; i<n; i++) {
             for (int j=0; j<m; j++) {
                 board[i][j] += dashboard[i][j];
-                if (board[i][j]>0)  count++;
+                if (board[i][j]>0) count++;
             }
         }
         
