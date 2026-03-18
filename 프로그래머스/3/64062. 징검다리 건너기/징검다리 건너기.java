63a2f1e93693930767c883f6fa6fc59bedb43ab4
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-03-18 10:21:58 +0900

    [level 3] Title: 징검다리 건너기, Time: 22.88 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -2,27 +2,30 @@ class Solution {
     public int solution(int[] stones, int k) {
         int answer = 0;
         
-        int low=1, high=2000_000_00;
+        int left = 1;
+        int right = 200_000_000;
         
-        while (low<=high) {
+        while (left<=right) {
             
-            int mid = (low+high)/2;
+            int mid = (left+right)/2;
             int empty = 0;
             
             for (int i=0; i<stones.length; i++) {
+                
                 if (stones[i]<mid) {
                     empty++;
-                    if (empty >= k) break;
+                    if (empty>=k) break;
                 }else {
                     empty=0;
                 }
+                
             }
             
             if (empty<k) {
                 answer = mid;
-                low = mid+1;
+                left = mid+1;
             }else {
-                high = mid-1;
+                right = mid-1;
             }
             
         }
