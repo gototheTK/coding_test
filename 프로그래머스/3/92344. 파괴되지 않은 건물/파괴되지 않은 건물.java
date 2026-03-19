2269211235cf13cc49785ac97cfe4a0230722e53
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-03-19 10:18:50 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 52.98 ms, Memory: 211 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -2,16 +2,16 @@ class Solution {
     public int solution(int[][] board, int[][] skill) {
         int answer = 0;
         
-        int count = 0;
-        
-        int m = board[0].length;
         int n = board.length;
+        int m = board[0].length;
         
         int[][] dashboard = new int[n+1][m+1];
+        
         
         for (int[] s : skill) {
             
             int type = s[0] == 1 ? -1 : 1;
+            
             int r1 = s[1], c1 = s[2];
             int r2 = s[3], c2 = s[4];
             int degree = type * s[5];
@@ -35,10 +35,12 @@ class Solution {
             }
         }
         
+        int count = 0;
+        
         for (int i=0; i<n; i++) {
             for (int j=0; j<m; j++) {
                 board[i][j] += dashboard[i][j];
-                if (board[i][j]>0) count++;
+                if (board[i][j]>0) count++; 
             }
         }
         
