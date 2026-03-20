1e11aa2313cc1220579cfe3dc158d04ee1395206
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-03-20 09:53:19 +0900

    [level 3] Title: 섬 연결하기, Time: 0.68 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -4,9 +4,10 @@ class Solution {
     
     public int find(int[] arr, int i) {
         
-        if (i==arr[i]) return i;
+        if (arr[i]==i) return i;
         
         return arr[i] = find(arr, arr[i]);
+        
     }
     
     public void union(int[] arr, int a, int b) {
@@ -26,31 +27,31 @@ class Solution {
         
     }
     
-    
     public int solution(int n, int[][] costs) {
         int answer = 0;
         
-        int[] visited = new int[n];
+        Arrays.sort(costs, (a, b) -> a[2]-b[2]);
         
+        int[] visited = new int[n];
         for (int i=0; i<n; i++) {
             visited[i] = i;
         }
         
-        Arrays.sort(costs, (a, b)->a[2]-b[2]);
-        int expense = 0;
-                    
+        int minExpense = 0;
+        
         for (int[] cost : costs) {
             
             int e1 = cost[0], e2 = cost[1];
+            int expense = cost[2];
             
             if (find(visited, e1) != find(visited, e2)) {
                 union(visited, e1, e2);
-                expense += cost[2];
+                minExpense += expense;
             }
             
         }
         
-        answer = expense;
+        answer = minExpense;
         
         return answer;
     }
