09f63c9b1e0e153e9290e41263d498d29ff6b65f
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-03-22 19:43:16 +0900

    [level 3] Title: 섬 연결하기, Time: 0.91 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,58 +1,58 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
-    
-    public int find(int[] arr, int i) {
-        
-        if (arr[i]==i) return i;
-        
+
+    private int find(int[] arr, int i) {
+
+        if (arr[i] == i) return i;
+
         return arr[i] = find(arr, arr[i]);
-        
     }
-    
-    public void union(int[] arr, int a, int b) {
-        
+
+    private void union(int[] arr, int a, int b) {
+
         int rootA = find(arr, a);
         int rootB = find(arr, b);
-        
+
         if (rootA != rootB) {
-            
-            if (rootA<rootB) {
+
+            if (rootA < rootB) {
                 arr[rootB] = rootA;
             }else {
                 arr[rootA] = rootB;
             }
-            
+
         }
-        
+
     }
-    
+
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        
-        Arrays.sort(costs, (a, b) -> a[2]-b[2]);
-        
+
         int[] visited = new int[n];
-        for (int i=0; i<n; i++) {
+
+        for (int i=0; i<n ; i++) {
             visited[i] = i;
         }
-        
-        int minExpense = 0;
-        
+
+        Arrays.sort(costs, (a, b) -> a[2]-b[2]);
+
+        int min = 0;
+
         for (int[] cost : costs) {
-            
-            int e1 = cost[0], e2 = cost[1];
-            int expense = cost[2];
-            
+
+            int e1 = cost[0], e2 = cost[1], expense = cost[2];
+
             if (find(visited, e1) != find(visited, e2)) {
                 union(visited, e1, e2);
-                minExpense += expense;
+                min += expense;
             }
-            
+
         }
         
-        answer = minExpense;
-        
+        answer = min;
+
         return answer;
     }
 }
