f8f4d5f8900e25e72bcee8bd251a762e233c398e
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-03-22 22:12:33 +0900

    [level 3] Title: 광고 삽입, Time: 337.35 ms, Memory: 321 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -6,7 +6,8 @@ class Solution {
         String answer = "";
 
         int playSeconds = convertTimeToSeconds(play_time);
-        int[] playCounts = new int[playSeconds+1];
+
+        int[] plays = new int[playSeconds+1];
 
         for (String log : logs) {
 
@@ -15,54 +16,61 @@ class Solution {
             int start = convertTimeToSeconds(times[0]);
             int end = convertTimeToSeconds(times[1]);
 
-            playCounts[start] += 1;
-            playCounts[end] -= -1;
+            plays[start] += 1;
+            plays[end] -= 1;
 
         }
 
-        for (int i=1; i<playCounts.length; i++) {
-            playCounts[i] -= playCounts[i-1];
+        for (int i=1; i<=playSeconds; i++) {
+            plays[i] += plays[i-1];
         }
 
         int advSeconds = convertTimeToSeconds(adv_time);
-        int count = 0;
-        int time = 0;
+        long maxSecondsSum = 0;
+        int maxStartSeconds = 0;
 
         for (int i=0; i<advSeconds; i++) {
-            count = Math.max(count, playCounts[i]);
+            maxSecondsSum += plays[i];
         }
+
+        long currentSecondsSum = maxSecondsSum;
 
         for (int i=advSeconds; i<=playSeconds; i++) {
 
-            if (playCounts[i] > count) {
-                time = i;
+            currentSecondsSum = currentSecondsSum + plays[i] - plays[i-advSeconds];
+
+            if (maxSecondsSum < currentSecondsSum) {
+                maxStartSeconds = i - advSeconds + 1;
+                maxSecondsSum = currentSecondsSum;
             }
 
         }
 
-        answer = convertSecondsToString(time);
+        answer = convertSecondsToTime(maxStartSeconds);
 
         return answer;
     }
 
     private int convertTimeToSeconds(String time) {
 
-        String[] splits = time.split(":");
-        int hours = Integer.parseInt(splits[0]);
-        int minutes = Integer.parseInt(splits[1]);
-        int seconds = Integer.parseInt(splits[2]);
+        String[] times = time.split(":");
+
+        int hours = Integer.parseInt(times[0]);
+        int minutes = Integer.parseInt(times[1]);
+        int seconds = Integer.parseInt(times[2]);
 
         return hours * 60 * 60 + minutes * 60 + seconds;
     }
 
-    private String convertSecondsToString(int seconds) {
+    private String convertSecondsToTime(int seconds) {
 
-        int hours = seconds / (60 *60);
+        int hours = seconds / (60 * 60);
         seconds %= (60 * 60);
         int minutes = seconds / 60;
         seconds %= 60;
 
         return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+
     }
 
 }
