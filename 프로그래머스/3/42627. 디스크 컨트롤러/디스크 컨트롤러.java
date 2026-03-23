b2ce0c917ca8d92f31d91546cc62af51fc89d483
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-03-23 09:11:31 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.42 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -6,32 +6,49 @@ class Solution {
         
         Arrays.sort(jobs, (a, b) -> a[0]-b[0]);
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1]-b[1]);
+        for (int i=0; i<jobs.length; i++) {
+            jobs[i] = new int[] {i, jobs[i][0], jobs[i][1]};
+        }
         
-        int time = 0;
-        int totalTime = 0;
-        int jobIndex = 0;
-        int count = 0;
-        
-        while (count < jobs.length) {
-            
-            while (jobIndex<jobs.length && jobs[jobIndex][0]<=time) {
-                pq.add(jobs[jobIndex]);
-                jobIndex++;
+        PriorityQueue<int[]> scheduler = new PriorityQueue<>((a, b) -> {
+           
+            if (a[2]==b[2]) {
+                
+                if (a[1]==b[1]) {
+                    return a[0]-b[0];
+                }
+                
+                return a[1]-b[1];
+                
             }
             
-            if (pq.isEmpty()) {
-                time = jobs[jobIndex][0];
+            return a[2]-b[2];
+        });
+        
+        int time = 0;
+        int period = 0;
+        int count = 0;
+        int index = 0;
+        
+        while (count<jobs.length) {
+            
+            while (index<jobs.length && time>=jobs[index][1]) {
+                scheduler.add(jobs[index]);
+                index++;
+            }
+            
+            if (scheduler.isEmpty()) {
+                time = jobs[index][1];
             }else {
-                int[] job = pq.poll();
-                time += job[1];
-                totalTime += time - job[0];
+                int[] job = scheduler.poll();
+                time += job[2];
+                period += time - job[1];
                 count++;
             }
             
         }
         
-        answer = totalTime / jobs.length;
+        answer = period / jobs.length;
         
         return answer;
     }
