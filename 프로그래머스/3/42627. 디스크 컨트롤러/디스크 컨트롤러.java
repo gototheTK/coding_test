866775922913f1b94cb00272a39c77b3fafa426f
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-03-25 08:51:16 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 2.57 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -6,49 +6,33 @@ class Solution {
         
         Arrays.sort(jobs, (a, b) -> a[0]-b[0]);
         
-        for (int i=0; i<jobs.length; i++) {
-            jobs[i] = new int[] {i, jobs[i][0], jobs[i][1]};
-        }
+        PriorityQueue<int[]> scheduler = new PriorityQueue<>((a, b) -> a[1]-b[1]);   
         
-        PriorityQueue<int[]> scheduler = new PriorityQueue<>((a, b) -> {
-           
-            if (a[2]==b[2]) {
-                
-                if (a[1]==b[1]) {
-                    return a[0]-b[0];
-                }
-                
-                return a[1]-b[1];
-                
-            }
-            
-            return a[2]-b[2];
-        });
         
         int time = 0;
-        int period = 0;
-        int count = 0;
+        int completed = 0;
         int index = 0;
+        int period = 0;
         
-        while (count<jobs.length) {
+        while (completed<jobs.length) {
             
-            while (index<jobs.length && time>=jobs[index][1]) {
+            while (index<jobs.length && jobs[index][0]<=time) {
                 scheduler.add(jobs[index]);
                 index++;
             }
             
             if (scheduler.isEmpty()) {
-                time = jobs[index][1];
+                time = jobs[index][0];
             }else {
-                int[] job = scheduler.poll();
-                time += job[2];
-                period += time - job[1];
-                count++;
+                int[] job = scheduler.remove();
+                time += job[1];
+                period += time - job[0];
+                completed++;
             }
             
         }
         
-        answer = period / jobs.length;
+        answer = period/jobs.length;
         
         return answer;
     }
