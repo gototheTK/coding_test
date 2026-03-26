dca12501180855f6a9a0f75262a6f0aae9e3e4f6
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-03-26 10:20:33 +0900

    [level 3] Title: 베스트앨범, Time: 8.36 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,45 +1,47 @@
 import java.util.*;
 import java.util.stream.*;
 
-
 class Solution {
-
     public int[] solution(String[] genres, int[] plays) {
-
         int[] answer = {};
-
-        Map<String, List<Integer>> genresMap = new HashMap<>();
-        Map<String, Integer> playsMap = new LinkedHashMap<>();
-
-        for (int i=0; i<genres.length; i++) {
-            genresMap.computeIfAbsent(genres[i], k -> new ArrayList<>()).add(i);
-            playsMap.put(genres[i], playsMap.getOrDefault(genres[i], 0) + plays[i]);
-        }
-
-        List<String> bestGenres = playsMap.entrySet().stream()
-                .sorted((a, b) -> b.getValue()-a.getValue())
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
         
-
-        List<Integer> bestAlbums = new ArrayList<>();
-
-        for (String genre : bestGenres) {
-
-            List<Integer> albums = genresMap.get(genre).stream()
-                    .sorted((a, b) -> plays[a]==plays[b] ? plays[a] : plays[b]-plays[a])
-                    .collect(Collectors.toList());
-
-            bestAlbums.add(albums.get(0));
-
-            if (albums.size()>1) bestAlbums.add(albums.get(1));
-
+        Map<String, List<Integer>> genresMap = new HashMap<>();
+        Map<String, Integer> playsSumMap = new HashMap<>();
+        
+        for (int i=0; i<genres.length; i++) {
+            
+            String genre = genres[i];
+            int play = plays[i];
+            
+            List<Integer> list = genresMap.computeIfAbsent(genre, k -> new ArrayList<>());
+            list.add(i);
+            
+            playsSumMap.put(genre, playsSumMap.getOrDefault(genre, 0) + play);
+            
         }
-
+        
+        List<String> sortedGenres = playsSumMap.entrySet().stream()
+            .sorted((e1, e2) -> e2.getValue() - e1.getValue())
+            .map(Map.Entry::getKey)
+            .collect(Collectors.toList());
+        
+        List<Integer> bestAlbums = new ArrayList<>();
+        
+        for (String genre : sortedGenres) {
+            
+            List<Integer> albums = genresMap.get(genre);
+            albums.sort((a, b) -> plays[b]-plays[a]);
+            
+            bestAlbums.add(albums.get(0));
+            
+            if (albums.size()>1) {
+                bestAlbums.add(albums.get(1));
+            }
+            
+        }
+        
         answer = bestAlbums.stream().mapToInt(Integer::intValue).toArray();
-
-        return  answer;
-
+        
+        return answer;
     }
-
 }
