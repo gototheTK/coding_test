4ea72b256ee60f5c4d462fe6abd4b573d80c0f89
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-03-27 10:43:02 +0900

    [level 2] Title: 구명보트, Time: 11.57 ms, Memory: 56.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        int left = 0;
+        int right = people.length-1;
+        
+        int count = 0;
+        
+        while (left <= right) {
+            
+            int total = people[left] + people[right];
+            
+            if(total<=limit) {
+                left++;
+                right--;
+            }else {
+                right--;
+            }
+            
+            count++;
+        }
+        
+        answer = count;
+        
+        return answer;
+    }
+}
