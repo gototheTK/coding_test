6c57b12d873b2579691d0a57de59217e57fbbfb7
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-04-02 10:08:41 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 29.31 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -2,26 +2,31 @@ class Solution {
     public boolean solution(int[][] key, int[][] lock) {
         boolean answer = false;
         
+        // 돌리는 로직과 일치하는 로직을 먼저 작성한다.
+        // 키를 자물쇠 영억을 벗어나, 걸칠 수 있도록, n + 2 (m-1)크기의 보드를 만든다.
+        // 키를 회전시키면서, (0, 0)부터 자물쇠를 위에서부터 아래로 움직여서 일치하는지 확인힌다. 이 때, 이모탭 방법을 사용한다.
+        // 위를 반복하면서 일치면 true 아니면 false를 반환한다.
+        
         int m = key.length;
         int n = lock.length;
+        int s = n + 2 * (m-1);
         
-        int size = n + (m-1) * 2;
-        
-        int[][] board = new int[size][size];
+        int[][] board = new int[s][s];
         
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
-                board[m-1+i][m-1+j] = lock[i][j];
+                board[i+(m-1)][j+(m-1)] = lock[i][j];
             }
         }
         
         for (int r=0; r<4; r++) {
             
-            board = rotate(board);
+            key = rotate(key);
             
-            for (int x=0; x<size-m+1; x++) {
+            for (int x=0; x<m-1+n; x++) {
                 
-                for (int y=0; y<size-m+1; y++) {
+                for (int y=0; y<m-1+n; y++) {
+                    
                     
                     for (int i=0; i<m; i++) {
                         for (int j=0; j<m; j++) {
@@ -31,29 +36,32 @@ class Solution {
                     
                     if (isMatched(board, m, n)) return true;
                     
+                    
                     for (int i=0; i<m; i++) {
                         for (int j=0; j<m; j++) {
                             board[x+i][y+j] -= key[i][j];
                         }
                     }
                     
+                    
                 }
                 
-            } 
+            }
             
-        }
+        } 
         
-        return false;
+        return answer;
     }
     
-    private int[][] rotate(int[][] board) {
+    private int[][] rotate(int[][] arr) {
         
-        int s = board.length;
-        int[][] result = new int[s][s];
+        int n = arr.length;
         
-        for (int i=0; i<s; i++) {
-            for (int j=0; j<s; j++ ) {
-                result[i][j] = board[j][s-1-i];
+        int[][] result = new int[n][n];
+        
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                result[j][n-i-1] = arr[i][j];
             }
         }
         
@@ -63,13 +71,14 @@ class Solution {
     
     private boolean isMatched(int[][] board, int m, int n) {
         
-        for (int i=m-1; i<m-1+n; i++) {
-            for (int j=m-1; j<m-1+n; j++) {
-                if (board[i][j]!=1) return false;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if (board[i+(m-1)][j+(m-1)] != 1) return false;
             }
         }
         
         return true;
-    } 
+        
+    }
     
 }
