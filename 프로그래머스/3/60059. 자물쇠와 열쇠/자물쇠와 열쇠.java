7e60342355f941344d94aafbec7f87cbbcbd83b6
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-04-02 10:36:14 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 29.23 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -2,11 +2,6 @@ class Solution {
     public boolean solution(int[][] key, int[][] lock) {
         boolean answer = false;
         
-        // 돌리는 로직과 일치하는 로직을 먼저 작성한다.
-        // 키를 자물쇠 영억을 벗어나, 걸칠 수 있도록, n + 2 (m-1)크기의 보드를 만든다.
-        // 키를 회전시키면서, (0, 0)부터 자물쇠를 위에서부터 아래로 움직여서 일치하는지 확인힌다. 이 때, 이모탭 방법을 사용한다.
-        // 위를 반복하면서 일치면 true 아니면 false를 반환한다.
-        
         int m = key.length;
         int n = lock.length;
         int s = n + 2 * (m-1);
@@ -15,7 +10,7 @@ class Solution {
         
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
-                board[i+(m-1)][j+(m-1)] = lock[i][j];
+                board[i+m-1][j+m-1] = lock[i][j];
             }
         }
         
@@ -27,28 +22,25 @@ class Solution {
                 
                 for (int y=0; y<m-1+n; y++) {
                     
-                    
                     for (int i=0; i<m; i++) {
                         for (int j=0; j<m; j++) {
-                            board[x+i][y+j] += key[i][j];
+                            board[i+x][j+y] += key[i][j];
                         }
                     }
                     
                     if (isMatched(board, m, n)) return true;
                     
-                    
                     for (int i=0; i<m; i++) {
                         for (int j=0; j<m; j++) {
-                            board[x+i][y+j] -= key[i][j];
+                            board[i+x][j+y] -= key[i][j];
                         }
                     }
-                    
                     
                 }
                 
             }
             
-        } 
+        }
         
         return answer;
     }
@@ -56,12 +48,11 @@ class Solution {
     private int[][] rotate(int[][] arr) {
         
         int n = arr.length;
-        
         int[][] result = new int[n][n];
         
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
-                result[j][n-i-1] = arr[i][j];
+                result[j][n-1-i] = arr[i][j];
             }
         }
         
@@ -73,12 +64,13 @@ class Solution {
         
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
-                if (board[i+(m-1)][j+(m-1)] != 1) return false;
+                
+                if (board[i+m-1][j+m-1]!=1) return false;
+                
             }
         }
         
         return true;
-        
     }
     
 }
