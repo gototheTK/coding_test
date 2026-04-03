1bb1329fcb69ec98e7ad31cabfe335cbfe1eaf13
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-04-03 12:31:07 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 67.66 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -5,51 +5,35 @@ class Solution {
     public int[] solution(String[] gems) {
         int[] answer = {};
         
-        /**
-        보석들의 종류와 종류수를 구해야한다.
-        
-        종류수 만큼의 슬라이딩 윈도우를 이용하여서, 처음부터 끝까지 확인한다.
-        
-        그 처리 중에서 보석이 다 구해지면, 시작점과 끝점 그리고 길이를 비교하면서 갱신한다.
-        
-        슬라이딩 윈도우를 다 마치면 해당 시작점과 끝점을 반환한다.
-        
-        **/
-        
-        int types = Arrays.stream(gems).collect(Collectors.toSet()).size();
+        int types = new HashSet<>(Arrays.asList(gems)).size();
         
         Map<String, Integer> typesMap = new HashMap<>();
         
         int left = 0;
-        int right = 0;
-        int minLen = gems.length+1;
+        int min = gems.length+1;
         
-        while (true) {
+        for (int right=0; right<gems.length; right++) {
             
-            int count = typesMap.size();
+            int rightCount = typesMap.getOrDefault(gems[right], 0)+1;
+            typesMap.put(gems[right], rightCount);
             
-            if (count == types) {
+            while (typesMap.size()==types) {
                 
-                if (minLen > right-left) {
-                    minLen = right-left;
-                    answer = new int[]{left+1, right};
+                if (min > right-left) {
+                    min = right-left;
+                    answer = new int[] {left+1, right+1};
                 }
                 
-                int gemsCount = typesMap.get(gems[left])-1;
+                int leftCount = typesMap.get(gems[left])-1;
                 
-                if (gemsCount==0) {
+                if(leftCount == 0) {
                     typesMap.remove(gems[left]);
                 }else {
-                    typesMap.put(gems[left], gemsCount);
+                    typesMap.put(gems[left], leftCount);
                 }
                 
                 left++;
                 
-            }else if (right == gems.length) {
-                break;
-            }else {
-                typesMap.put(gems[right], typesMap.getOrDefault(gems[right], 0)+1);
-                right++;
             }
             
         }
