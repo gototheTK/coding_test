e2ea3c39cdb9238bfe5c3db115bf8416b8a02f7f
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-04-03 11:32:07 +0900

    [level 3] Title: 단속카메라, Time: 12.93 ms, Memory: 59.2 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -4,24 +4,28 @@ class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
         
-        Arrays.sort(routes, (a, b)->a[1]-b[1]);
+        /**
+        진출시간이 짧은 순으로 정렬한다.
+        차량들을 하나씩 꺼내어서, 전의 진출시간보다 진입시간이 낮은 것들은 넘어가고, 진출 시간이 큰 것이 나오면 비교시간을 갱신한다. 그리고 +1을 한다.
+        **/
         
+        Arrays.sort(routes, (a,b)->a[1]-b[1]);
+        
+        int camera = 1;
         int position = routes[0][1];
-        int encountered = 1;
         
         for (int[] route : routes) {
             
-            int entry = route[0];
-            int exit = route[1];
+            int entry = route[0], exit = route[1];
             
-            if (entry > position) {
+            if (entry>position) {
                 position = exit;
-                encountered++;
+                camera++;
             }
             
         }
         
-        answer = encountered;
+        answer = camera;
         
         return answer;
     }
