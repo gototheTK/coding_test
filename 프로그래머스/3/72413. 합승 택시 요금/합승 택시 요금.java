95f38cd0b95b30c31547129fa1dd503db725748f
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-04-06 10:50:00 +0900

    [level 3] Title: 합승 택시 요금, Time: 39.32 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -6,28 +6,32 @@ class Solution {
         
         int[] dist = new int[n+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[start] = 0;
+        
+        boolean[] visited = new boolean[n+1];
         
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o->o[1]));
+        dist[start] = 0;
         pq.add(new int[]{start, dist[start]});
         
-        while (!pq.isEmpty()) {
+        while(!pq.isEmpty()) {
             
-            int[] fromFare = pq.poll();
+            int[] way = pq.poll();
             
-            int from = fromFare[0];
-            int fromCost = fromFare[1];
+            int from = way[0];
+            int fromCost = way[1];
             
-            if (dist[from] < fromCost) continue;
+            if (fromCost>dist[from]) continue;
             
-            for (int[] toFare : graph[from]) {
+            for (int[] route : graph[from]) {
                 
-                int to = toFare[0];
-                int toCost = toFare[1];
+                int to = route[0];
+                int toCost = route[1];
                 
-                if (dist[to] > fromCost + toCost) {
-                    dist[to] = fromCost + toCost;
-                    pq.add(new int[] {to, dist[to]});
+                int cost = fromCost + toCost;
+                
+                if (dist[to] > cost) {
+                    dist[to] = cost;
+                    pq.add(new int[] {to, cost});
                 }
                 
             }
@@ -41,19 +45,25 @@ class Solution {
     public int solution(int n, int s, int a, int b, int[][] fares) {
         int answer = 0;
         
+        /**
+        그래프를 만든다.
+        S, A, B에서 시작하여 다익스트라를 구한다.
+        S + A + B의 합이 최소가 되는 지점의 합을 구한다.
+        **/
+        
         List<int[]>[] graph = new ArrayList[n+1];
         
-        for (int i=0; i<=n; i++) {
+        for (int i=1; i<=n; i++) {
             graph[i] = new ArrayList<>();
         }
         
         for (int[] fare : fares) {
-            int from = fare[0];
-            int to = fare[1];
+            int start = fare[0];
+            int end = fare[1];
             int cost = fare[2];
             
-            graph[from].add(new int[]{to, cost});
-            graph[to].add(new int[]{from, cost});
+            graph[start].add(new int[]{end, cost});
+            graph[end].add(new int[]{start, cost});
         }
         
         int[] distS = dijkstra(n, s, graph);
@@ -64,7 +74,7 @@ class Solution {
         
         for (int i=1; i<=n; i++) {
             
-            if (distS[i] != Integer.MAX_VALUE && distA[i] != Integer.MAX_VALUE && distB[i] != Integer.MAX_VALUE) {
+            if (distS[i]!=Integer.MAX_VALUE && distA[i]!=Integer.MAX_VALUE && distB[i]!=Integer.MAX_VALUE) {
                 min = Math.min(min, distS[i] + distA[i] + distB[i]);
             }
             
