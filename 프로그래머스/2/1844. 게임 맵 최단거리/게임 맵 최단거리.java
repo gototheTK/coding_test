6b60ce70c549ca5fd7c7ae56403f881fe1447dee
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-04-06 12:15:48 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.94 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,47 +1,49 @@
 import java.util.*;
 
 class Solution {
-
     public int solution(int[][] maps) {
-
         int answer = -1;
-
-        int n = maps.length;
-        int m = maps[0].length;
-
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
+        
         Queue<int[]> queue = new ArrayDeque<>();
+        maps[0][0] = 0;
         queue.add(new int[]{0, 0, 1});
-        maps[0][0]=0;
-
+        
+        int n = maps[0].length-1;
+        int m = maps.length-1;
+        
+        
         while (!queue.isEmpty()) {
-
-            int[] c = queue.poll();
-
-            if (c[0]==n-1 && c[1]==m-1) {
-                answer = c[2];
-                break;
+            
+            int[] coordinate = queue.poll();
+            
+            int x = coordinate[0];
+            int y = coordinate[1];
+            int c = coordinate[2];
+            
+            if (x==n && y==m) return c;
+            
+            if (x+1 <= n && maps[y][x+1]==1) {
+                maps[y][x+1] = 0;
+                queue.add(new int[]{x+1, y, c+1});
             }
-
-            for (int i=0; i<4; i++) {
-
-                int x = c[0] + dx[i]; 
-                int y = c[1] + dy[i];
-                int move = c[2];
-
-                if (x>=0 && x<n && y>=0 && y<m && maps[x][y]==1) {
-                    maps[x][y]=0;
-                    queue.add(new int[]{x, y, move+1});
-                }
-
+            
+            if (y+1 <= m && maps[y+1][x]==1) {
+                maps[y+1][x] = 0;
+                queue.add(new int[]{x, y+1, c+1});
             }
+            
+            if (x-1 >= 0 && maps[y][x-1]==1) {
+                maps[y][x-1] = 0;
+                queue.add(new int[]{x-1, y, c+1});
+            }
+            
+            if (y-1 >= 0 && maps[y-1][x]==1) {
+                maps[y-1][x] = 0;
+                queue.add(new int[]{x, y-1, c+1});
+            }
+            
         }
-
-
+        
         return answer;
-
     }
-
 }
