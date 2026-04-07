925feca3a28436485e60ff4c70c8eadfaaae9b95
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-04-07 11:14:19 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 10.56 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,33 +1,28 @@
 class Solution {
     
-    private void divideAndConquer(int[][] arr, int len, int x, int y, int[] result) {
+    private void divideAndConquer(int[][] arr, int size, int x, int y, int[] result) {
         
-        if (isCompressed(arr, len, x, y)) {
+        if (isCompressed(arr, size, x, y)) {
             result[arr[x][y]]++;
             return;
         }
         
-        int half = len/2;
+        int half = size/2;
         
         divideAndConquer(arr, half, x, y, result);
         divideAndConquer(arr, half, x+half, y, result);
         divideAndConquer(arr, half, x, y+half, result);
         divideAndConquer(arr, half, x+half, y+half, result);
-        
     }
     
-    private boolean isCompressed(int[][] arr, int len, int x, int y) {
+    private boolean isCompressed(int[][] arr, int size, int x, int y) {
         
         int previous = arr[x][y];
         
-        for (int i=x; i<len+x; i++) {
-            
-            for (int j=y; j<len+y; j++) {
-                
+        for (int i=x; i<size+x; i++) {
+            for (int j=y; j<size+y; j++) {
                 if (arr[i][j]!=previous) return false;
-                
             }
-            
         }
         
         return true;
@@ -36,6 +31,13 @@ class Solution {
     
     public int[] solution(int[][] arr) {
         int[] answer = {};
+        
+        /**
+        1. 배열의 길이의 절반이루어진 사각형을 검사한다.
+        2. 전부 같은 숫자면, 0, 1의 개수를 올린다.
+        3. 사격형의 길이의 절반을 구해서, 그 길이 만큼의 사각형들을 검사한다.
+        4. 전부 같은 숫잔 내지는 길이가 1일 때 까지 위를 반복한다.
+        */
         
         int[] result = new int[2];
         
