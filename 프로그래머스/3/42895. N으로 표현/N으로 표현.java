6936c29258e9ede2b10470ab372060a6d8d37a7f
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-04-07 12:00:49 +0900

    [level 3] Title: N으로 표현, Time: 16.46 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -26,7 +26,15 @@ class Solution {
             
             list.get(i).add(Integer.parseInt(String.valueOf(N).repeat(i)));
             
-            for (int j=1; j<i; j++) {
+            /**
+                i가 i/2가 아닌이유
+                A-B != B-A, A/B != B/A이기 때문이다.
+                가령, 5-555 != 555-5 ,5/555 != 555/5인 경우가 반드시 발생할 것이다.
+                그렇기 때문에, 이 경우를 모두 아우를 수 있게 i/2가 아니라 i가 맞다.
+                그래도 i/2를 쓰고 싶다면, 뺄셈, 나눗셈을 할 때, 한쪽이 큰경우 작은 경우도 연산하여 넣어주어야한다.
+            **/
+            
+            for (int j=1; j<=i/2; j++) {
                 
                 int k = i-j;
                 
@@ -35,10 +43,13 @@ class Solution {
                     for (int num2 : list.get(k)) {
                         
                         list.get(i).add(num1 + num2);
-                        list.get(i).add(num1 - num2);
                         list.get(i).add(num1 * num2);
                         
+                        list.get(i).add(num1 - num2);
+                        list.get(i).add(num2 - num1);
+                        
                         if (num2>0) list.get(i).add(num1 / num2);
+                        if (num1>0) list.get(i).add(num2 / num1);
                         
                     }
                     
