a52e54ddaf224d3b4e5f7162eda9ad40a036f294
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-04-09 16:32:43 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 20.77 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -3,40 +3,55 @@ import java.util.*;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
-        // 다리를 나타내는 큐. 다리 길이를 0(빈 공간)으로 가득 채우고 시작합니다.
-        Queue<Integer> bridge = new LinkedList<>();
+
+        /**
+        
+        트럭이든 공백이든 빼낸다.
+        
+        트럭이면, 하중을 줄인다.
+        
+        둘 중 뭘하든 다리의 하중을 측정하여서 트럭을 보내든, 공백을 보낸다.
+        
+        위를 인덱스가 끝까지 도달할 때 까지, 반복한다.
+        
+        인덱스의 끝에 도달하면 빼기만한다.
+        
+        **/
+        
+        Queue<Integer> bridge = new ArrayDeque<>();
         
         for (int i=0; i<bridge_length; i++) {
             bridge.add(0);
         }
         
         int time = 0;
-        int currentWeight = 0;
         int index = 0;
+        int totalWeight = 0;
         
-        // 대기 중인 트럭이 모두 다리에 올라갈 때까지 반복합니다.
-        while (index < truck_weights.length) {
-            time++;
+        while (!bridge.isEmpty()) {
+
+            int truck = bridge.poll();
             
-            // 1. 시간이 1초 흐르면, 다리 맨 앞의 공간(또는 트럭)이 무조건 빠져나갑니다.
-            currentWeight -= bridge.poll();
+            totalWeight -= truck;
             
-            // 2. 대기 중인 다음 트럭이 올라갈 수 있는지 하중을 계산합니다.
-            if (currentWeight + truck_weights[index] <= weight) {
-                // 올라갈 수 있으면 트럭을 올리고 하중을 더합니다.
-                bridge.add(truck_weights[index]);
-                currentWeight += truck_weights[index];
-                index++;
-            }else {
-                // 하중 때문에 못 올라가면, 빈 공간(0)을 밀어 넣어서 기존 트럭들만 전진시킵니다.
-                bridge.add(0);
+            if (index<truck_weights.length) {
+                
+                if (totalWeight + truck_weights[index] <= weight) {
+                    totalWeight += truck_weights[index];
+                    bridge.add(truck_weights[index]);
+                    index++;
+                }else {
+                    bridge.add(0);
+                }
+                
             }
+            
+            time++;
             
         }
         
-        // while문이 끝난 시점은 "마지막 트럭이 다리에 막 진입한(올라탄) 1초 뒤"입니다.
-        // 마지막 트럭이 다리를 완전히 빠져나오려면 다리의 길이만큼의 시간이 더 필요합니다.
-        answer = time + bridge_length;
+        answer = time;
+        
         
         return answer;
     }
