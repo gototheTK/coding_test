028006d428e458dfc7c2ab3590589f5cd7b681fd
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-04-10 11:52:49 +0900

    [level 2] Title: 배달, Time: 5.34 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,10 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int[][] road, int K) {
-        int answer = 1;
-        
-        if (N==1) return answer;
-        
+        int answer = 0;
+
         List<int[]>[] graph = new ArrayList[N+1];
         int[] dist = new int[N+1];
         
@@ -16,13 +14,13 @@ class Solution {
         
         for (int[] route : road) {
             int from = route[0], to = route[1], cost = route[2];
-            graph[from].add(new int[]{to, cost});
-            graph[to].add(new int[]{from, cost});
+            graph[from].add(new int[] {to, cost});
+            graph[to].add(new int[] {from, cost});
         }
         
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o->o[1]));
         dist[1] = 0;
-        pq.add(new int[]{1, 0});
+        pq.add(new int[] {1,0});
         
         while (!pq.isEmpty()) {
             
@@ -31,15 +29,14 @@ class Solution {
             int from = current[0];
             int fromCost = current[1];
             
-            if (fromCost > dist[from]) continue;
-            
+            if (dist[from]<fromCost) continue;
             
             for (int[] next : graph[from]) {
                 
                 int to = next[0];
                 int toCost = fromCost + next[1];
                 
-                if (dist[to] > toCost) {
+                if (dist[to]>toCost) {
                     dist[to] = toCost;
                     pq.add(new int[] {to, toCost});
                 }
@@ -51,9 +48,8 @@ class Solution {
         int count = 0;
         
         for (int i=1; i<=N; i++) {
-            if (dist[i]<=K) {
+            if (dist[i]<=K)
                 count++;
-            }
         }
         
         answer = count;
