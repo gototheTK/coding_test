ba2d776635d7bdd050166abe04aec9d2c0469735
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-04-14 17:19:11 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 69.73 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -6,23 +6,23 @@ class Solution {
         int[] answer = {};
         
         int types = Arrays.stream(gems).collect(Collectors.toSet()).size();
-        
         Map<String, Integer> selected = new HashMap<>();
-        int minLen = gems.length+1;
+        
         int left = 0;
+        int minLen = gems.length+1;
         
         for (int right = 0; right<gems.length; right++) {
             
-            selected.put(gems[right], selected.getOrDefault(gems[right], 0) + 1);
+            selected.put(gems[right], selected.getOrDefault(gems[right], 0)+1);
             
-            while (selected.size() == types) {
+            while (types == selected.size()) {
                 
                 if (minLen > right-left) {
-                    minLen = right-left;
+                    minLen = right - left;
                     answer = new int[] {left+1, right+1};
                 }
                 
-                int count = selected.get(gems[left])-1;
+                int count = selected.get(gems[left]) - 1;
                 
                 if (count==0) {
                     selected.remove(gems[left]);
@@ -31,7 +31,6 @@ class Solution {
                 }
                 
                 left++;
-                
             }
             
         }
