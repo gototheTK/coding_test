6768070cbdac606dfef00b243ebb7e9ce36a640f
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-04-14 11:35:53 +0900

    [level 2] Title: 배달, Time: 4.67 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,9 +1,10 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-
+        
         List<int[]>[] graph = new ArrayList[N+1];
         int[] dist = new int[N+1];
         
@@ -13,30 +14,34 @@ class Solution {
         }
         
         for (int[] route : road) {
-            int from = route[0], to = route[1], cost = route[2];
+            
+            int from = route[0];
+            int to = route[1];
+            int cost = route[2];
+            
             graph[from].add(new int[] {to, cost});
             graph[to].add(new int[] {from, cost});
+            
         }
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o->o[1]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1]-o2[1]);
         dist[1] = 0;
-        pq.add(new int[] {1,0});
+        pq.add(new int[] {1, dist[1]});
         
         while (!pq.isEmpty()) {
             
             int[] current = pq.poll();
-            
             int from = current[0];
             int fromCost = current[1];
             
-            if (dist[from]<fromCost) continue;
+            if (dist[from] < fromCost) continue;
             
             for (int[] next : graph[from]) {
                 
                 int to = next[0];
                 int toCost = fromCost + next[1];
                 
-                if (dist[to]>toCost) {
+                if (dist[to] > toCost) {
                     dist[to] = toCost;
                     pq.add(new int[] {to, toCost});
                 }
@@ -48,8 +53,9 @@ class Solution {
         int count = 0;
         
         for (int i=1; i<=N; i++) {
-            if (dist[i]<=K)
-                count++;
+            
+            if (dist[i]<=K) count++;
+            
         }
         
         answer = count;
