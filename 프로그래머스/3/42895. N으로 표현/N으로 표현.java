91f96a82bc2647c0dad50a3cb32eb32a74a7c28b
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-04-23 14:13:05 +0900

    [level 3] Title: N으로 표현, Time: 31.75 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -5,7 +5,15 @@ class Solution {
         int answer = -1;
         
         if (N == number) return 1;
-            
+        
+        /**
+        1. 이전의 연산결과에 사칙연산을 한 결과를 새로만들어 저장한다.
+        2. 이렇게 최대 8번까지하고, 없다면 -1을 리턴한다.
+        3. 사칙연산 중에 0으로 나누지 않도록 주의한다.
+        
+        자료구조는 List와 Set을 사용한다.
+        **/
+        
         List<Set<Integer>> list = new ArrayList<>();
         
         for (int i=0; i<=8; i++) {
@@ -16,27 +24,29 @@ class Solution {
         
         for (int i=2; i<=8; i++) {
             
-            list.get(i).add(Integer.parseInt(String.valueOf(N).repeat(i)));
+            int num = Integer.parseInt(String.valueOf(N).repeat(i));
+            
+            list.get(i).add(num);
             
             for (int j=1; j<=i/2; j++) {
                 
                 int k = i-j;
                 
                 for (int num1 : list.get(j)) {
-                    
+                
                     for (int num2 : list.get(k)) {
-                    
+
                         list.get(i).add(num1 + num2);
                         list.get(i).add(num1 * num2);
                         
                         list.get(i).add(num1 - num2);
                         list.get(i).add(num2 - num1);
                         
-                        if (num2>0) list.get(i).add(num1 / num2);
-                        if (num1>0) list.get(i).add(num2 / num1);
-                        
+                        if (num1!=0) list.get(i).add(num2/num1);
+                        if (num2!=0) list.get(i).add(num1/num2);
+
                     }
-                    
+
                 }
                 
             }
@@ -44,7 +54,8 @@ class Solution {
             if (list.get(i).contains(number)) return i;
             
         }
-            
+        
+        
         return answer;
     }
 }
