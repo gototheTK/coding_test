a33b2304ad6a6de8ef624592ac9d0612c954f9b8
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-05-21 13:14:39 +0900

    [level 2] Title: 기능개발, Time: 1.87 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -4,42 +4,40 @@ class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer = {};
         
-        Queue<int[]> queue = new ArrayDeque<>();
+        Deque<Integer> progress = new ArrayDeque<>();
+        Deque<Integer> speed = new ArrayDeque<>();
         
         for (int i=0; i<progresses.length; i++) {
-            queue.add(new int[] {progresses[i], speeds[i]});
+            progress.add(progresses[i]);
+            speed.add(speeds[i]);
         }
         
         List<Integer> result = new ArrayList<>();
         
-        while (!queue.isEmpty()) {
+        int time = 0;
+        int count = 0;
+        
+        while (!progress.isEmpty()) {
             
-            int size = queue.size();
+            int p = progress.getFirst();
+            int s = speed.getFirst();
             
-            for (int i=0; i<size; i++) {
-                int[] jobs = queue.poll();
-                queue.add(new int[] {jobs[0] + jobs[1], jobs[1]});
+            if (p + s * time >= 100) {
+                progress.poll();
+                speed.poll();
+                count++;
+            }else if(count > 0) {
+                result.add(count);
+                count = 0;
+            }else {
+                time++;
             }
-            
-            int count = 0;
-            
-            for (int i=0; i<size; i++) {
-                int[] jobs = queue.peek();
-                
-                if (jobs[0] >= 100) {
-                    count++;
-                    queue.poll();
-                }else {
-                    break;
-                }
-                
-            }
-            
-            result.add(count);
             
         }
         
-        answer = result.stream().mapToInt(Integer::valueOf).filter(n->n>0).toArray();
+        result.add(count);
+        
+        answer = result.stream().mapToInt(Integer::valueOf).toArray();
         
         return answer;
     }
