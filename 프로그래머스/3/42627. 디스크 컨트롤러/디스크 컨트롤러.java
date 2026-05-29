138bc39d5176f5cf5d8d05a81ea41095fff17622
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-05-29 21:20:05 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.34 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,38 +1,49 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 class Solution {
+
     public int solution(int[][] jobs) {
         int answer = 0;
-        
-        Arrays.sort(jobs, (a, b) -> a[0]-b[0]);
-        
-        PriorityQueue<int[]> queue = new PriorityQueue<>((a,b) -> a[1]-b[1]);
-        
-        int count = 0;
+
+        Arrays.sort(jobs, Comparator.comparingInt(job -> job[0]));
+
+        Queue<int[]> heap = new PriorityQueue<>((job1, job2) -> {
+
+            if (job1[1]==job2[1]){
+
+                return Integer.compare(job1[0], job2[0]);
+
+            }
+
+            return Integer.compare(job1[1], job2[1]);
+
+        });
+
         int index = 0;
         int time = 0;
-        int periord = 0;
-        
-        while (count < jobs.length) {
+
+        while (index < jobs.length || !heap.isEmpty()) {
             
-            while (index<jobs.length && jobs[index][0] <= time) {
-                queue.add(jobs[index]);
+            while (index < jobs.length && time >= jobs[index][0]) {
+                heap.add(jobs[index]);
                 index++;
             }
             
-            if (queue.isEmpty()) {
+            if (heap.isEmpty()) {
                 time = jobs[index][0];
-            }else {
-                int[] job = queue.remove();
-                time += job[1];
-                periord += time - job[0];
-                count++;
+                continue;
             }
+            
+            int[] job = heap.poll();
+            time += job[1];
+            answer += (time - job[0]);
             
         }
         
-        answer = periord/jobs.length;
-        
-        return answer;
+        return answer/jobs.length;
+
     }
 }
