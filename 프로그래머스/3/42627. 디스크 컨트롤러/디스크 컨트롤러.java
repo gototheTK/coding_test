8d91fb7da55c9be5fcd14370dd69ac01140c1aaf
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-06-02 04:06:42 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.61 ms, Memory: 65.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,22 +1,18 @@
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
-import java.util.Queue;
-
-class Solution {
+import java.util.Queue;class Solution {
 
     public int solution(int[][] jobs) {
+
         int answer = 0;
 
         Arrays.sort(jobs, Comparator.comparingInt(job -> job[0]));
 
-        Queue<int[]> heap = new PriorityQueue<>((job1, job2) -> {
 
-            if (job1[1]==job2[1]){
+        Queue<int[]> queue = new PriorityQueue<>((job1, job2) -> {
 
-                return Integer.compare(job1[0], job2[0]);
-
-            }
+            if (job1[1] == job2[1]) return Integer.compare(job1[0], job2[0]);
 
             return Integer.compare(job1[1], job2[1]);
 
@@ -25,25 +21,28 @@ class Solution {
         int index = 0;
         int time = 0;
 
-        while (index < jobs.length || !heap.isEmpty()) {
-            
+        while (index < jobs.length || !queue.isEmpty()) {
+
             while (index < jobs.length && time >= jobs[index][0]) {
-                heap.add(jobs[index]);
+                queue.add(jobs[index]);
                 index++;
             }
-            
-            if (heap.isEmpty()) {
+
+            if (queue.isEmpty()) {
                 time = jobs[index][0];
                 continue;
             }
-            
-            int[] job = heap.poll();
+
+            int[] job = queue.poll();
             time += job[1];
-            answer += (time - job[0]);
-            
+            answer += time - job[0];
+
         }
         
-        return answer/jobs.length;
+        answer/=jobs.length;
+
+        return answer;
 
     }
+
 }
