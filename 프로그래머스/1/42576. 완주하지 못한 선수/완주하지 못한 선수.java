1ff24d87d239385cbe684367d145b8bba7a09bab
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-06-29 15:29:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 54.73 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,27 +1,29 @@
 import java.util.*;
 
 class Solution {
-    public String solution(String[] participant, String[] completion) {
+   public String solution(String[] participant, String[] completion) {
         String answer = "";
-        
-        Map<String, Integer> participants = new HashMap<>();
-        
+
+        Map<String, Integer> map = new HashMap<>();
+
         for (String person : participant) {
-            participants.put(person, participants.getOrDefault(person, 0)+1);
+            map.put(person, map.getOrDefault(person, 0) + 1);
         }
-        
+
         for (String person : completion) {
-            int count = participants.getOrDefault(person, 0)-1;
-            
-            if (count<=0) {
-                participants.remove(person);
+
+            int count = map.getOrDefault(person, 0) - 1;
+
+            if (count <= 0) {
+                map.remove(person);
             }else {
-                participants.put(person, count);
+                map.put(person, count);
             }
+
         }
-        
-        answer = new ArrayList<>(participants.keySet()).get(0);
-        
+
+        answer = map.keySet().toArray(String[]::new)[0];
+
         return answer;
     }
 }
