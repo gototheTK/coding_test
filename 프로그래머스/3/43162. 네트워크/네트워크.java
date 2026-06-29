5b63ccaad973ea2c0c20f682bd2ba5d836959e29
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-06-29 20:10:28 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,55 +1,36 @@
 class Solution {
-    
-    private int find(int[] arr, int i) {
-        
-        if (arr[i]==i) return i;
-        
-        return arr[i] = find(arr, arr[i]);
-        
-    }
-    
-    private void union(int[] arr, int a, int b) {
-        
-        int rootA = find(arr, a);
-        int rootB = find(arr, b);
-        
-        if (rootA != rootB) {
-            
-            if (rootA < rootB) {
-                arr[rootB] = rootA;  
-            }else {
-                arr[rootA] = rootB;
-            }
-            
-        }
-        
-    }
-    
+
     public int solution(int n, int[][] computers) {
         int answer = 0;
         
-        int count = n;
-        int[] group = new int[n];
-        
-        for (int i=0; i<n; i++) {
-            group[i] = i;
-        }
+        boolean[] visited = new boolean[n];
         
         for (int i=0; i<n; i++) {
             
-            for (int j=0; j<n; j++) {
-                
-                if (find(group, i) != find(group, j) && computers[i][j]==1) {
-                    union(group, i, j);
-                    count--;
-                }
-                
+            if (!visited[i]) {
+                dfs(computers, visited, i);
+                answer++;
             }
             
         }
         
-        answer = count;
-        
         return answer;
     }
+    
+    public void dfs(int[][] computers, boolean[] visited, int start) {
+        
+        if (visited[start]) return;
+        
+        visited[start] = true;
+        
+        for (int i=0; i<computers[start].length; i++) {
+            
+            if (i != start && computers[start][i]==1) {
+                dfs(computers, visited, i);
+            }
+            
+        }
+        
+    }
+    
 }
