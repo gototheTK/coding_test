25236d6061f3fc69e410a23681ed24f4c202e1e2
gototheTK <48229162+gototheTK@users.noreply.github.com>
2026-06-29 20:11:00 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,36 +1,82 @@
+import java.util.*;
+
 class Solution {
+
+    public void union(int u, int v, int[] disjoint){
+
+        /**
+         int[][] computers= {
+                {1,0,0,0,0,0,1},
+                {0,1,1,0,1,0,0},
+                {0,1,1,1,0,0,0},
+                {0,0,1,1,0,0,0},
+                {0,1,0,0,1,1,0},
+                {0,0,0,0,1,1,1},
+                {1,0,0,0,0,1,1}
+        };
+        int n = 7;
+        **/
+
+        u = find(u,disjoint);
+        v = find(v,disjoint);
+
+        for(int i=0; i<disjoint.length; i++){
+            if (disjoint[i] == u || disjoint[i] == v){
+                disjoint[i] = u > v ? v : u;
+            }
+        }
+
+    }
+
+    public int find(int v, int[] disjoint) {
+
+        /**
+         int[][] computers= {
+                {1,0,0,0,0,0,1},
+                {0,1,1,0,1,0,0},
+                {0,1,1,1,0,0,0},
+                {0,0,1,1,0,0,0},
+                {0,1,0,0,1,1,0},
+                {0,0,0,0,1,1,1},
+                {1,0,0,0,0,1,1}
+        };
+        int n = 7;
+        **/
+
+        if(disjoint[v]==v){
+            return v;
+        }else{
+            return disjoint[v] = find(disjoint[v],disjoint);
+        }
+
+    }
 
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        
-        boolean[] visited = new boolean[n];
-        
-        for (int i=0; i<n; i++) {
-            
-            if (!visited[i]) {
-                dfs(computers, visited, i);
-                answer++;
-            }
-            
+        Set<Integer> set = new HashSet<>();
+        int[] disjoint = new int[n];
+        for(int i=0; i<n; i++){
+            disjoint[i] = i;
         }
-        
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+
+                if(computers[i][j]==1&&find(i,disjoint)!=find(j,disjoint)){
+                    union(i,j,disjoint);
+                }
+
+            }
+
+        }
+
+
+
+        for(int i=0; i<n; i++) {
+            set.add(disjoint[i]);
+        }
+        answer = set.size();
+
         return answer;
     }
-    
-    public void dfs(int[][] computers, boolean[] visited, int start) {
-        
-        if (visited[start]) return;
-        
-        visited[start] = true;
-        
-        for (int i=0; i<computers[start].length; i++) {
-            
-            if (i != start && computers[start][i]==1) {
-                dfs(computers, visited, i);
-            }
-            
-        }
-        
-    }
-    
 }
